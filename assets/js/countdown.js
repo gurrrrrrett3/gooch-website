ad6923f4f3bb0b52c5b3167bf359ec97020550d7
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2022-01-09 22:05:45 -0500

    Fix incorrect date and fix end timer

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,20 +1,26 @@
 const countdownObject = document.getElementById('countdown');
 
-const until = 1641964320000
+const until = 1641965040000
 
 const countdown = setInterval(() => {
-    const now = Date.now()
-    const diff = until - now
+    let now = Date.now()
+    let diff = until - now
 
     console.log(diff);
 
-    const days = f(Math.floor(diff / (1000 * 60 * 60 * 24)));
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
     const hours = f(Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     const minutes = f(Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)));
     const seconds = f(Math.floor((diff % (1000 * 60)) / 1000));
     const milliseconds = f2(Math.floor((diff % (1000 * 60)) / 10)).substr(2);
 
-    countdownObject.innerHTML = `${days}:${hours}:${minutes}:${seconds}.${milliseconds}`;
+    countdownObject.innerHTML = diff > 0 ? `${days}d ${hours}h ${minutes}m ${seconds}s ${milliseconds}ms` : '0d 0h 0m 0s 0ms';
+
+    if (diff < 0) {
+        clearInterval(countdown);
+        countdownObject.innerHTML = '0d 0h 0m 0s 0ms';
+        countdownObject.style.color = 'red';
+    }
 }, 10);
 
 function f(n) {
