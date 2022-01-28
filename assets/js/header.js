318e8529778a4a6bc821d0c9c9847fb3dafd0a79
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2022-01-28 16:11:57 -0500

    homepage meta tags

---
+++ assets/js/header.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", (ev) => {
+
+    let pageTitle = document.head.
+    console.log(document.head.title);
+
+    document.head.innerHTML += `
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+`
+
+
+})
