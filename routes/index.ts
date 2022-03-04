18b44f479c325887323e85bd0fa35404705b16a7
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2022-03-03 21:45:35 -0500

    qr

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,12 @@
-import { Router } from 'express';
+import { Router } from "express";
 import path from "path";
 
-import assetRouter from './assets';
-import plethoraRouter from './plethora';
-import sfbRouter from './sfb'
-import apiRouter from './api';
-import userRouter from './user';
+import assetRouter from "./assets";
+import plethoraRouter from "./plethora";
+import sfbRouter from "./sfb";
+import apiRouter from "./api";
+import userRouter from "./user";
+import qrRouter from "./qr";
 
 let router = Router();
 
@@ -14,22 +15,23 @@ router.use("/plethora", plethoraRouter);
 router.use("/sfb", sfbRouter);
 router.use("/api", apiRouter);
 router.use("/user", userRouter);
+router.use("/qr", qrRouter);
 
 router.get("/", (req, res) => {
-    res.sendFile(path.resolve("./assets/html/index.html"));
-})
+  res.sendFile(path.resolve("./assets/html/index.html"));
+});
 
 router.get("/skyblock", (req, res) => {
-    res.sendFile(path.resolve("./assets/html/skyblock.html"));
-})
+  res.sendFile(path.resolve("./assets/html/skyblock.html"));
+});
 
 router.get("/appeal", (req, res) => {
-    res.sendFile(path.resolve("./assets/html/appeal.html"));
-    //res.redirect("/assets/local/mp3/connection")
-})
+  res.sendFile(path.resolve("./assets/html/appeal.html"));
+  //res.redirect("/assets/local/mp3/connection")
+});
 
 router.get("/goat", (req, res) => {
-    res.redirect("https://goatse.ru")
-})
+  res.redirect("https://goatse.ru");
+});
 
 export default router;
