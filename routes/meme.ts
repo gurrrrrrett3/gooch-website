6a631d8b7d00a43c579ab025f01b808c644a67d4
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2022-06-01 15:47:44 -0400

    Update meme.ts

--- routes/meme.ts
+++ routes/meme.ts
@@ -15,7 +15,7 @@ router.get("/ip/:ip", async (req, res) => {
     ctx.font = "48px Arial"
     ctx.fillStyle = "black"
     
-    ctx.fillText("nice arguement",150, 250)
+    ctx.fillText("nice argument",150, 250)
     ctx.fillText(`but ${req.params.ip}`,150, 300)
 
     res.setHeader("Content-Type", "image/png")
