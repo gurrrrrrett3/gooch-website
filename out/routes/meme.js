bb34d8be9877e2f3e6de37f9d0b4bd10cdced918
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2022-06-01 16:04:26 -0400

    poggers

--- out/routes/meme.js
+++ out/routes/meme.js
@@ -16,16 +16,41 @@ const express_1 = require("express");
 const canvas_1 = __importDefault(require("canvas"));
 const path_1 = __importDefault(require("path"));
 const router = (0, express_1.Router)();
-router.get("/ip/:ip", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const canvas = new canvas_1.default.Canvas(1248, 720);
-    const ctx = canvas.getContext("2d");
-    const img = yield canvas_1.default.loadImage(path_1.default.resolve("./assets/png/intruder.png"));
-    ctx.drawImage(img, 0, 0);
-    ctx.font = "48px Arial";
-    ctx.fillStyle = "black";
-    ctx.fillText("nice arguement", 150, 250);
-    ctx.fillText(`but ${req.params.ip}`, 150, 300);
+router.get("/ip", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const qs = req.query;
+    const lines = [qs.a, qs.b, qs.c, qs.d, qs.e, qs.f, qs.g, qs.h, qs.i, qs.j, qs.k, qs.l, qs.m, qs.n, qs.o, qs.p, qs.q, qs.r, qs.s, qs.t, qs.u, qs.v, qs.w, qs.x, qs.y, qs.z].map((v) => v === null || v === void 0 ? void 0 : v.toString());
+    let cleanLines = lines.filter((v) => v != undefined);
+    if (cleanLines.length == 0) {
+        cleanLines = [req.ip];
+    }
+    console.log(cleanLines);
+    //@ts-ignore
+    const buf = yield renderIPImage(...cleanLines);
     res.setHeader("Content-Type", "image/png");
-    res.send(canvas.toBuffer());
+    res.send(buf);
 }));
+router.get("/ip/:ip", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const buf = yield renderIPImage(req.params.ip);
+    res.setHeader('Content-Type', 'image/png');
+    res.send(buf);
+}));
+function renderIPImage(...lines) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const canvas = new canvas_1.default.Canvas(1248, 720);
+        const ctx = canvas.getContext("2d");
+        const img = yield canvas_1.default.loadImage(path_1.default.resolve("./assets/png/intruder.png"));
+        ctx.drawImage(img, 0, 0);
+        ctx.font = "48px Arial";
+        ctx.fillStyle = "black";
+        ctx.fillText("nice argument", 150, 250);
+        ctx.fillText(`but ${lines[0]}`, 150, 300);
+        let y = 350;
+        for (let i = 1; i < lines.length; i++) {
+            ctx.fillText(lines[i], 150, y);
+            y += 50;
+        }
+        const buf = canvas.toBuffer();
+        return buf;
+    });
+}
 exports.default = router;
--- routes/meme.ts
+++ routes/meme.ts
@@ -4,8 +4,30 @@ import path from "path"
 
 const router = Router();
 
+router.get("/ip", async (req, res) => {
+    const qs = req.query
+    const lines = [qs.a, qs.b, qs.c, qs.d, qs.e, qs.f, qs.g, qs.h, qs.i, qs.j, qs.k, qs.l, qs.m, qs.n, qs.o, qs.p, qs.q, qs.r, qs.s, qs.t, qs.u, qs.v, qs.w, qs.x, qs.y, qs.z].map((v) => v?.toString())
+
+    let cleanLines = lines.filter((v) => v != undefined)
+    if (cleanLines.length == 0) {
+        cleanLines = [req.ip]
+    }
+    
+    console.log(cleanLines)
+    //@ts-ignore
+    const buf = await renderIPImage(...cleanLines)
+    res.setHeader("Content-Type", "image/png")
+    res.send(buf)
+})
+
 router.get("/ip/:ip", async (req, res) => {
 
+    const buf = await renderIPImage(req.params.ip)
+    res.setHeader('Content-Type', 'image/png')
+    res.send(buf)
+})
+
+async function renderIPImage(...lines: string[] ) {
     const canvas = new Canvas.Canvas(1248, 720) 
     const ctx = canvas.getContext("2d")
 
@@ -16,10 +38,15 @@ router.get("/ip/:ip", async (req, res) => {
     ctx.fillStyle = "black"
     
     ctx.fillText("nice argument",150, 250)
-    ctx.fillText(`but ${req.params.ip}`,150, 300)
+    ctx.fillText(`but ${lines[0]}`,150, 300)
+    let y = 350
+    for (let i = 1; i < lines.length; i++) {
+        ctx.fillText(lines[i],150, y)
+        y += 50
+    }
 
-    res.setHeader("Content-Type", "image/png")
-    res.send(canvas.toBuffer())
-})
+    const buf = canvas.toBuffer()
+    return buf
+}
 
 export default router;
